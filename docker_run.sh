8f3b83fdca566ec43fa35498000cca6716a983b8
dev-369 <63476064+dev-369@users.noreply.github.com>
2020-05-03 03:01:37 +0530

    uploading required files.

---
+++ docker_run.sh
@@ -0,0 +1,11 @@
+
+
+python3 dev_fig_font.py
+tput setaf 5
+sudo service docker start 
+tput setaf 4
+sudo docker-compose up -d
+tput setaf 6
+sudo docker ps
+tput setaf 5
+sudo docker logs mydocker-compose_wordpressos_1
